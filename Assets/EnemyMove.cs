815eb0bce8bf44613f4cd69d1baedbdd860cfaaf
Markus <markus_riener@gmx.at>
2018-01-18 18:11:32 +0100

    Added enemy

---
+++ Assets/EnemyMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+
+
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class EnemyMove : MonoBehaviour {
+
+    public Transform Player;
+
+    private Rigidbody rigid;
+
+	// Use this for initialization
+	void Start () {
+        rigid = GetComponent<Rigidbody>();
+
+        Player = GameObject.Find("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 dir = Player.position - this.transform.position;
+
+        dir.Normalize();
+
+        rigid.velocity = dir;
+	}
+}
---
+++ Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour
+{
+    public GameObject enemy;
+
+    [Range(0.5f, 10f)]
+    public float spawnInterval = 2;
+
+    private float timer = 0;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > spawnInterval)
+        {
+            Spawn();
+            timer = 0;
+        }
+    }
+
+    void Spawn()
+    {
+        Vector3 spawnPosition = transform.position;
+
+        //spawnPosition.x = -10 + (Random.value * 20);
+        spawnPosition.x = Random.Range(2, 15);
+
+        Instantiate(enemy, spawnPosition, transform.rotation);
+    }
+}
